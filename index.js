037558cfadbca13cca08c1c7debb1ce4551e7742
rizahoemae <humaida.mawaddatur@gmail.com>
2025-01-30 00:00:18 +0700

    feat: add image logo (nikon, canon) & fix layout drawtext

---
+++ index.js
@@ -0,0 +1,11 @@
+const Logo = {
+  canon: "canon.png",
+  fujifilm: "fujifilm.png",
+  nikon: "nikon.png",
+  sony: "sony.png",
+  apple: "apple.png",
+  samsumg: "samsumg.png",
+  gopro: "gopro.png",
+  xiaomi: "xiaomi.png",
+};
+export default Logo;
